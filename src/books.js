35c1bf7326e062bc24692fa5db2cb31e85cf7e8a
Yutman <kuriapeter95@gmail.com>
2024-03-14 11:01:34 +0300

    First commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const data = [
+{
+    id: 1,
+    img:  'https://images-na.ssl-images-amazon.com/images/I/81mpSoJzv4L._AC_UL600_SR600,400_.jpg',
+    author: 'Amelia Hepworth',
+    title: 'I Love You to the Moon and Back'
+},
+{
+    id: 2,
+    img:  'https://images-na.ssl-images-amazon.com/images/I/71Pu10o+WLL._AC_UL600_SR600,400_.jpg',
+    author: 'Robinne Lee',
+    title: 'The Idea of You: A Novel'
+},
+{
+    id: 3,
+    img:  'https://images-na.ssl-images-amazon.com/images/I/611X8GI7hpL._AC_UL600_SR600,400_.jpg',
+    author: 'Robert Greene',
+    title: 'The 48 Laws of Power'
+},
+];
+
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// CSS
+import './index.css';
+
+// books
+import {data} from './books';
+import SpecificBook from './book';
+import { greeting } from './testing/testing';
+
+
+function BookList () {
+    console.log(greeting);
+    return (
+        <section className='booklist'>
+            {data.map((book, index)=>{
+            return (
+                <SpecificBook key={book.id} {...book}></SpecificBook>
+            );  
+            })}
+        </section>
+    );
+};
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
